955122fcd6f492ccbccc75f0e6942d7745be00fe
DanLomanto <daniellomanto@gmail.com>
2015-07-06 15:38:12 -0400

    Fixing a bug when a post back was happening

--- FishingHole/ManageFriends.aspx.cs
+++ FishingHole/ManageFriends.aspx.cs
@@ -28,23 +28,88 @@ namespace FishingHole
 
 		private void LoadFriends()
 		{
-			Friend person = new Friend
+			#region data setup
+
+			Friend person1 = new Friend
+			{
+				ID = 1,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+			Friend person2 = new Friend
+			{
+				ID = 2,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+			Friend person3 = new Friend
 			{
 				ID = 3,
 				FirstName = "Dan",
 				LastName = "Lomanto",
 				Email = "something@gmail.com"
 			};
+			Friend person4 = new Friend
+			{
+				ID = 4,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+
+			Friend person5 = new Friend
+			{
+				ID = 5,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+			Friend person6 = new Friend
+			{
+				ID = 6,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+			Friend person7 = new Friend
+			{
+				ID = 7,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
+			Friend person8 = new Friend
+			{
+				ID = 8,
+				FirstName = "Dan",
+				LastName = "Lomanto",
+				Email = "something@gmail.com"
+			};
 
 			List<Friend> Friends = new List<Friend>();
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
-			Friends.Add(person);
+			Friends.Add(person1);
+			Friends.Add(person2);
+			Friends.Add(person3);
+			Friends.Add(person4);
+			Friends.Add(person5);
+			Friends.Add(person6);
+			Friends.Add(person7);
+			Friends.Add(person8);
+
+			#endregion data setup
+
+			int numberOfThreads = FriendsList.Controls.Count;
+			for (int i = 0; i < numberOfThreads; i++)
+			{
+				if (FriendsList.HasControls())
+				{
+					FriendsList.Controls.RemoveAt(0);
+				}
+				else
+				{ break; }
+			}
 
 			int friendCounter = 0;
 			foreach (Friend friend in Friends)
