47d1ca3f1c36429023e52beec8822d41a4adcdf0
DanLomanto <daniellomanto@gmail.com>
2015-07-13 13:46:18 -0400

    adding a method comment

--- FishingHole/ShareLocation.aspx.cs
+++ FishingHole/ShareLocation.aspx.cs
@@ -28,6 +28,11 @@ namespace FishingHole
 			LocationSharedNotification.Visible = false;
 		}
 
+		/// <summary>
+		/// Handles the Click event of the ShareLocationWithFriends control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
 		protected void ShareLocationWithFriends_Click(object sender, EventArgs e)
 		{
 			List<string> formValidationErrors = new List<string>();
